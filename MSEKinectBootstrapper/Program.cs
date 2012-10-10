15b8e081c1a757c2d5a730ee5189f5368e7608df
Teddy Seyed <teddy.seyed@gmail.com>
2012-10-10 16:36:35 -0600

    Added bootstrapper application for testing purposes

---
+++ MSEKinectBootstrapper/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using MSEKinect;
+
+namespace MSEKinectBootstrapper
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            MSEKinectManager km = new MSEKinectManager();
+            km.Start();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
