bdbb4c1096c48343c32e0b33a9a58e6f96f21073
Patrick King <patrick.f.king@gmail.com>
2012-11-15 12:07:54 -0700

    Added 3 second timeout to pairing, for people and devices.

--- MSEKinect/Pairing/PairableDevice.cs
+++ MSEKinect/Pairing/PairableDevice.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using MSELocator;
 using System.Runtime.CompilerServices;
+using System.Timers;
+
 
 [assembly: InternalsVisibleTo("MSEKinectTests")]
 
@@ -12,13 +14,26 @@ namespace MSEKinect
 
     public class PairableDevice : Device
     {
+        private const int TIMEOUT_TIME = 3000; // miliseconds
+
         private PairingState _pairingState;
         public PairingState PairingState
         {
             get { return _pairingState; }
-            set { _pairingState = value; }
+            set 
+            {
+                if (value == PairingState.PairingAttempt)
+                {
+                    pairingTimeoutTimer = new Timer(TIMEOUT_TIME);
+                    pairingTimeoutTimer.Elapsed += pairingTimeout;
+                    pairingTimeoutTimer.AutoReset = false;
+                    pairingTimeoutTimer.Start();
+                }
+                _pairingState = value;
+            }
         }
 
+        private Timer pairingTimeoutTimer;
 
         public override string ToString()
         {
@@ -28,6 +43,23 @@ namespace MSEKinect
                 HeldByPersonIdentifier,
                 PairingState); 
         }
+
+
+        /// <summary>
+        /// Handler for the pairing timeout timer, which sets us to 'not pairing' if we haven't paired yet.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pairingTimeout(Object sender, ElapsedEventArgs e)
+        {
+            // Only reset our pairing state if we haven't paired, and if this timer is the most recent timer created
+            if (_pairingState == PairingState.PairingAttempt && sender == pairingTimeoutTimer)
+            {
+                this.PairingState = PairingState.NotPaired;
+                pairingTimeoutTimer = null;
+            }
+        }
+
         
     }
 }
--- MSEKinect/Pairing/PairablePerson.cs
+++ MSEKinect/Pairing/PairablePerson.cs
@@ -3,17 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MSELocator;
+using System.Timers;
+
 
 namespace MSEKinect
 {
     public class PairablePerson : Person
     {
+        private const int TIMEOUT_TIME = 3000; // miliseconds
+
         private PairingState _pairingState;
         public PairingState PairingState
         {
             get { return _pairingState; }
-            set { _pairingState = value; }
+            set
+            {
+                if (value == PairingState.PairingAttempt)
+                {
+                    pairingTimeoutTimer = new Timer(TIMEOUT_TIME);
+                    pairingTimeoutTimer.Elapsed += pairingTimeout;
+                    pairingTimeoutTimer.AutoReset = false;
+                    pairingTimeoutTimer.Start();
+                }
+                _pairingState = value;
+            }
         }
+
+
+        private Timer pairingTimeoutTimer;
 
         public override string ToString()
         {
@@ -23,6 +40,22 @@ namespace MSEKinect
                 HeldDeviceIdentifier,
                 PairingState);
         }
+
+        /// <summary>
+        /// Handler for the pairing timeout timer, which sets us to 'not pairing' if we haven't paired yet.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pairingTimeout(Object sender, ElapsedEventArgs e)
+        {
+            // Only reset our pairing state if we haven't paired, and if this timer is the most recent timer created
+            if (_pairingState == PairingState.PairingAttempt && sender == pairingTimeoutTimer)
+            {
+                this.PairingState = PairingState.NotPaired;
+                pairingTimeoutTimer = null;
+            }
+        }
+
 
     }
 }
