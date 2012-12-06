0115c0c1c6a287096f25e464410d86719c1bc6b2
Daniel Sabourin <sabourid@ucalgary.ca>
2012-12-06 11:01:36 -0700

    Integrating MSEAPI-CS into MSEAPI as per @patrickking request

---
+++ MSEAPI-CS/MSEMultiSurface.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Collections;
+using MSEAPI_CS_Routes;
+using MSEAPI_CS.Models;
+using IntAirAct;
+
+namespace MSEAPI_CS
+{
+    class MSEMultiSurface
+    {
+        public ArrayList recievedImageHandlers;
+        public ArrayList recievedDataHandlers;
+
+        public bool isRunning;
+
+        private IAIntAirAct intAirAct;
+        private static TraceSource logger;
+       
+
+        public MSEMultiSurface()
+        {
+            //Setup Logging
+            logger = new TraceSource("MSEMultiSurface");
+
+            this.isRunning = false;
+
+            //Initialize Our Arrays of Handlers
+            //this.recievedImageHandlers
+
+
+            //Setup IntAirAct
+            this.intAirAct = IAIntAirAct.New();
+
+#if DEBUG
+            this.intAirAct.Port = 12345;
+#endif
+
+            //Setup Pairing Recognizer
+
+
+
+
+            //Setup Locator
+
+
+
+
+
+            //Setup MSE Standard Routes
+            //this.setupRoutes();
+            //this.setupGestureHandlers();
+
+        }
+
+        public bool start()
+        {
+            if (this.isRunning)
+            {
+                return true;
+            } else {
+                return false;
+            }
+
+        }
+
+        /// <summary>
+        /// Stops the Multi-surface Environment
+        /// </summary>
+        public void stop()
+        {
+            this.intAirAct.Stop();
+        }
+
+        public void setupRoutes()
+        {
+            //Setup Received Data Handlers
+            this.setupDataRoute();
+            this.setupImageRoute();
+        }
+
+
+        public void setupDataRoute()
+        {
+            //Setup Data Route
+            this.intAirAct.Route(Routes.DataRoute, delegate(IARequest request, IAResponse response)
+            {
+                //Retrieves the data from the request
+                byte[] data = request.Body;
+
+                //Retrieves gesture from request
+                //MSEGesture gesture = this.gestureFromMetadata(request.Metadata);
+
+                //Error Handling
+
+
+                //Run Handlers
+            });
+
+            
+        }
+
+        public void setupImageRoute()
+        {
+            //Setup Image Route
+            this.intAirAct.Route(Routes.ImageRoute, delegate(IARequest request, IAResponse response)
+            {
+                //Retrieve the name of the image
+                string imageName = request.Parameters["imagename"];
+
+                //Retrieve gesture from request
+                //MSEGesture gesture = this.gestureFromMetadata(request.Metadata);
+
+                //Retrieve the image
+
+
+                //Error handling
+
+                //Run Handlers
+
+                
+
+            });
+        }
+
+        #region Locator Methods
+
+        public delegate void MSESingleDeviceHandler(MSEDevice device);
+        public delegate void MSEDeviceCollectionHandler(List<MSEDevice> devices);
+        public delegate void MSEErrorHandler(Exception error);
+
+        public void locate(MSEDevice device, MSESingleDeviceHandler success, MSEErrorHandler failure) {
+            IARequest deviceRequest = new IARequest(Routes.GetDeviceInfoRoute);
+            deviceRequest.Parameters["identifier"] = device.Identifier;
+
+            IEnumerable devicesSupportingRoutes = this.intAirAct.DevicesSupportingRoute(Routes.GetDeviceInfoRoute);
+
+            foreach (IADevice iaDevice in devicesSupportingRoutes)
+            {
+                this.intAirAct.SendRequest(deviceRequest, iaDevice, delegate(IAResponse response, Exception exception) {
+                    if (exception != null)
+                    {
+                        failure(exception);
+                        return;
+                    }
+                    else
+                    {
+
+                        //MSEDevice returnDevice = new MSEDevice();
+                        //Dictionary<string,string> returnBody = 
+
+ //                       returnDevice.Identifier = returnBody["identifier"];
+
+                        success(response.BodyAs<IADevice>());
+                    }
+                });
+            }
+        }
+
+
+        #endregion
+    }
+}
---
+++ MSEAPI-CS/Models/MSEDevice.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using IntAirAct;
+
+namespace MSEAPI_CS.Models
+{
+    class MSEDevice
+    {
+
+        public string Identifier
+        {
+            get;
+            set;
+        }
+
+        public double Orientation
+        {
+            get;
+            set;
+        }
+
+        public Point Location
+        {
+            get;
+            set;
+        }
+
+        public DateTime LastUpdated
+        {
+            get;
+            set;
+        }
+
+        public IADevice NetworkDevice
+        {
+            get;
+            set;
+        }
+
+
+
+
+    }
+}
